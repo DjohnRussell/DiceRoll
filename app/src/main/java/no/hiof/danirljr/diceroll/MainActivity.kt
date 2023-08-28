fc5786daacc07b4b945c82d3ddb28bcfa295db10
danieljohnrussell <danieljr@hiof.no>
2023-08-28 20:40:44 +0200

    Cleaning up default codes
    fjerner generert kode, samt tilpasser riktig logokk

--- app/src/main/java/no/hiof/danirljr/diceroll/MainActivity.kt
+++ app/src/main/java/no/hiof/danirljr/diceroll/MainActivity.kt
@@ -3,10 +3,6 @@ package no.hiof.danirljr.diceroll
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -17,30 +13,19 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DiceRollTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                DiceRollerApp()
             }
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun DiceRollerApp(){
+    DiceWithButtonAndImage()
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    DiceRollTheme {
-        Greeting("Android")
-    }
+fun DiceWithButtonAndImage( modifier: Modifier = Modifier) {
+
 }
